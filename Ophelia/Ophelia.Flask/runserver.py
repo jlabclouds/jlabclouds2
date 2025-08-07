8401c3ec382b7d32c883b880cab80b44fbe16b50
jlabclouds <jlabcloud@outlook.com>
2025-08-07 17:39:46 -0400

    update Ophelia

---
+++ Ophelia/Ophelia.Flask/runserver.py
@@ -0,0 +1,8 @@
+import os
+import logging
+import flask
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
